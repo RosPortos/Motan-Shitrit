00393349d55da7b0e2b06fc53b49aca3793a5490
WebDev <nehaiki@ukr.net>
2023-01-13 07:55:26 +0200

    add block form and pagination

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ const revDel = require('gulp-rev-delete-original');
 const htmlmin = require('gulp-htmlmin');
 const gulpif = require('gulp-if');
 const notify = require('gulp-notify');
-const image = require('gulp-image');
+//const image = require('gulp-image');
 const { readFileSync } = require('fs');
 const concat = require('gulp-concat');
 
@@ -105,7 +105,7 @@ const images = () => {
     './src/img/**/*.jpeg',
     './src/img/**/*.svg'
   ])
-    .pipe(gulpif(isProd, image()))
+    //.pipe(gulpif(isProd, image()))
     .pipe(dest('./app/img'));
 };
 
